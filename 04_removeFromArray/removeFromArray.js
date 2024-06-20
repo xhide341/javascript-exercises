01fde4dc2c7ab62ed84daf3cc2e84b10b58b56bf
xhide341 <shawnehgn10@gmail.com>
2024-06-20 22:49:34 +0800

    fix 04_removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    return arr.filter(item => !valuesToRemove.includes(item));
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
