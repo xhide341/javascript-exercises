cfcc25bf0087100b0a143efc2650f05d70375c53
xhide341 <shawnehgn10@gmail.com>
2024-06-20 22:09:34 +0800

    fix 03_reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const textArray = text.split("");
+    let output = '';
+    for (let i = textArray.length - 1; i >= 0; i--) {
+        output += textArray[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
