f803da0d2662aa36e928f5cc19960263a34eb1a6
xhide341 <shawnehgn10@gmail.com>
2024-06-20 21:58:49 +0800

    fix 02_repeatstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let text = '';
+    if (number >= 0) {
+        for (i = 0; i < number; i++) {
+            text += string;
+        }
+    } else {
+        text = 'ERROR';
+    }
 
+    return text;
 };
 
 // Do not edit below this line
