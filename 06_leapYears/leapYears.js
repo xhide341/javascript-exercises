6293d25694ffa0174dc004b87f4e59556885600b
xhide341 <shawnehgn10@gmail.com>
2024-06-21 10:51:18 +0800

    fix 06_leapyears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // divisible by 400 and 4
+    if (year % 4 === 0 && !(year % 100 === 0) || year % 400 === 0 ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
