ce422347d257df9f5d236ba0a78dc80d50a650e2
xhide341 <shawnehgn10@gmail.com>
2024-06-21 11:20:43 +0800

    fix 07_tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let result = (farenheit - 32) * 5/9;
+  return result = checkResult(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let result = (celcius * 9/5) + 32;
+  return result = checkResult(result);
 };
+
+function checkResult(result) {
+  if (Number.isInteger(result)) {
+    return Math.round(result);
+  } else {
+    return parseFloat(result.toFixed(1));
+  }
+}
 
 // Do not edit below this line
 module.exports = {
