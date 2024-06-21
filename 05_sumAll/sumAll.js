e64fb1a2c628686da20f0b98038cb64cf5415fd2
xhide341 <shawnehgn10@gmail.com>
2024-06-21 10:35:30 +0800

    fix 05_sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, lastNum) {
+    let result = 0;
+    if (typeof(startNum) === 'number' && typeof(lastNum) === 'number' && startNum >= 0 && lastNum >= 0) {
+        if (startNum > lastNum) {
+            for (let i = startNum; i >= lastNum; i--) {
+                result += i;
+            }
+        } else if (startNum < lastNum) {
+            for (let i = startNum; i <= lastNum; i++) {
+                result += i;
+            }
+        } else {
+            return 'ERROR';
+        }
+        return result;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
