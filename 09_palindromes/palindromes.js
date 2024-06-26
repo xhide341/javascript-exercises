0da767383b1608548166f73dc3596801d771919a
xhide341 <shawnehgn10@gmail.com>
2024-06-26 19:28:17 +0800

    finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    const cleanString = string.replace(/[^a-zA-Z0-9]/g, '');
 
+    const reversedString = cleanString.split('').reverse().join('');
+
+    return cleanString.toLowerCase() === reversedString.toLowerCase();
 };
 
 // Do not edit below this line
