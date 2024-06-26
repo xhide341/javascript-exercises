00d653074373f97f45031b8c756a89808e3ff1bd
xhide341 <shawnehgn10@gmail.com>
2024-06-26 22:04:51 +0800

    finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+// num is the Nth number of the sequence
 
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    
+    let current = 0;
+    let prev = 1;
+
+    for (i = 0; i < num; i++) {
+        let next = current + prev;
+        prev = current;
+        current = next;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
