281fe95ae1a69d834f402e96d672b2eac37c5539
xhide341 <shawnehgn10@gmail.com>
2024-06-26 19:03:20 +0800

    finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => total += current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total *= current);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let result = 1;
+  for (let i = 0; i < num2; i++) {
+    result *= num1;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (i = 1; i <= num; i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
