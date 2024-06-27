5d787ccd3047c5c3d61e85cba2718a6cf47199e8
xhide341 <shawnehgn10@gmail.com>
2024-06-27 18:37:52 +0800

    finish 12_findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, person) => {
+      const currentAge = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+      const oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+      
+      return currentAge > oldestAge ? person : oldest;
+    });
+  }
 // Do not edit below this line
 module.exports = findTheOldest;
